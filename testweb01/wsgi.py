0ccef0168439467c8167065b578ca1693310ce5c
GabrielLin2022 <sam90044@yahoo.com.tw>
2022-11-14 20:16:07 +0800

    Add code

--- testweb01/wsgi.py
+++ testweb01/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testweb01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testweb01.settings')#environ.setdefault設定為初始環境，'testweb01的資料夾，下的settings檔案'，架到伺服器上時，用自動帶入這個路徑
 
 application = get_wsgi_application()
